6509db6c18c232a99f3f3447a83d54bbc6121cc4
aaron <aaronbushman@gmail.com>
2019-08-29 16:17:29 -0400

    initial files

---
+++ schema.sql
@@ -0,0 +1,6 @@
+-- Drops the blogger if it exists currently --
+DROP DATABASE IF EXISTS burgers;
+-- Creates the "blogger" database --
+CREATE DATABASE burgers;
+---mysql server using burgers db
+USE burgers;
